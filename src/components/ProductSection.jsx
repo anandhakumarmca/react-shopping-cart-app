673ecf9476730356e4bd2bbd764823e6966107c4
Anandh Kumar <accetanandhmca@gmail.com>
2023-08-10 01:03:50 +0530

    updated

--- src/components/ProductSection.jsx
+++ src/components/ProductSection.jsx
@@ -1,62 +1,61 @@
 import React from "react";
 import ProductCard from "./ProductCard";
 import "./ProductSection.css"; // Import your CSS file
-
 const ProductSection = ({ addToCart, removeFromCart }) => {
   // Example product data
   const products = [
     {
       id: 1,
-      image: "./Images/ponniyin-selvan-comics-1.jpg",
+      image: "https://github.com/anandhakumarmca/react-shopping-cart-app/blob/main/src/components/Images/ponniyin-selvan-comics-1.jpg",
       title: "Pudhu Vellam - Aadi Thirunaal",
       price: "RS.199.00",
       isSale: false,
     },
     {
       id: 2,
-      image: "./Images/ponniyin-selvan-comics-2.jpg",
+      image: "https://github.com/anandhakumarmca/react-shopping-cart-app/blob/main/src/components/Images/ponniyin-selvan-comics-2.jpg",
       title: "Pudhu Vellam - Vinnagra Kovil",
       price: "RS.199.00",
       isSale: true,
     },
     {
       id: 3,
-      image: "./Images/ponniyin-selvan-comics-3.jpg",
+      image: "https://github.com/anandhakumarmca/react-shopping-cart-app/blob/main/src/components/Images/ponniyin-selvan-comics-3.jpg",
       title: "Pudhu Vellam - Kadambur Maligai",
       price: "RS.199.00",
       isSale: false,
     },
     {
       id: 4,
-      image: "./Images/ponniyin-selvan-comics-4.jpg",
+      image: "https://github.com/anandhakumarmca/react-shopping-cart-app/blob/main/src/components/Images/ponniyin-selvan-comics-4.jpg",
       title: "Pudhu Vellam - Nadunisi Kootam",
       price: "RS.199.00",
       isSale: true,
     },
     {
       id: 5,
-      image: "./Images/ponniyin-selvan-comics-5.jpg",
+      image: "https://github.com/anandhakumarmca/react-shopping-cart-app/blob/main/src/components/Images/ponniyin-selvan-comics-5.jpg",
       title: "Pudhu Vellam - Pallakkil yaar",
       price: "RS.199.00",
       isSale: false,
     }, 
     {
       id: 6,
-      image: "./Images/ponniyin-selvan-comics-6.jpg",
+      image: "https://github.com/anandhakumarmca/react-shopping-cart-app/blob/main/src/components/Images/ponniyin-selvan-comics-6.jpg",
       title: "Pudhu Vellam - Nandhini",
       price: "RS.199.00",
       isSale: true,
     },
     {
       id: 7,
-      image: "./Images/ponniyin-selvan-comics-7.jpg",
+      image: "https://github.com/anandhakumarmca/react-shopping-cart-app/blob/main/src/components/Images/ponniyin-selvan-comics-7.jpg",
       title: "Pudhu Vellam - Kudanthai Jothidar",
       price: "RS.199.00",
       isSale: true,
     },
     {
       id: 8,
-      image: "./Images/ponniyin-selvan-comics-8.jpg",
+      image: "https://github.com/anandhakumarmca/react-shopping-cart-app/blob/main/src/components/Images/ponniyin-selvan-comics-8.jpg",
       title: "Pudhu Vellam - Thidum Prevesam",
       price: "RS.199.00",
       isSale: true,
